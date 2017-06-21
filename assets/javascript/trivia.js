b2af8da530f019a493290c71c2efad3d702482f5
mccalldan <drm8266@gmail.com>
2017-06-20 23:47:14 -0400

    submit

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -3,31 +3,13 @@ $(document).ready(function() {
 $("#start").on("click", function () {
 	$("#start").toggle();
 //Then shows timer
-function pretty_time_string(num) {
-    return ( num < 10 ? "0" : "" ) + num;
-  }
+function(startTimer){
+}
+// Then shows first question
 
-var start = new Date;    
-
-setInterval(function() {
-  var total_seconds = (new Date - start) / 1000;   
-
-  var hours = Math.floor(total_seconds / 3600);
-  total_seconds = total_seconds % 3600;
-
-  var minutes = Math.floor(total_seconds / 60);
-  total_seconds = total_seconds % 60;
-
-  var seconds = Math.floor(total_seconds);
-
-  hours = pretty_time_string(hours);
-  minutes = pretty_time_string(minutes);
-  seconds = pretty_time_string(seconds);
-
-  var currentTimeString = hours + ":" + minutes + ":" + seconds;
-
-  $('.timer').text(currentTimeString);
-}, 1000);
+var questions = ["How many countries in the World?", "Who is the current US President (2017)?", "Where does the Pope live?"];
+var corrAnswers = ["165", "Donald Trump", "Italy"];
+var counter = 30;
 
 
 
